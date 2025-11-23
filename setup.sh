905687a180caa74c385745bb65e5cbd0dc768091
Sander Möller <69638788+mollersander@users.noreply.github.com>
2025-11-23 16:17:41 +0200

    make ansible use stow to link dotfiles to user home dir

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+#########################################
+# Install Ansible (Debian/Ubuntu)
+#########################################
+echo "[INFO] Installing Ansible..."
+sudo apt update
+sudo apt install -y software-properties-common ansible
+
+#########################################
+# Run the playbook
+#########################################
+echo "[INFO] Running Ansible setup playbook..."
+ansible-playbook ansible/playbooks/setup.yml
+
+echo "[INFO] Done."
